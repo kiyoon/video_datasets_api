3dc1ddf890a5fb06ac76cbe84d80a3ad7e2025ce
Kiyoon Kim <yoonkr33@gmail.com>
2021-04-20 22:18:08 +0900

    new generate labels faster than original CATER repo

--- video_datasets_api/cater/class_keys.py
+++ video_datasets_api/cater/class_keys.py
@@ -31,20 +31,24 @@ OBJECTS = ['sphere', 'spl', 'cylinder', 'cube', 'cone']
 
 
 
-def action_order_unique(classes):
-    def reverse(el):
-        if el == ('during',):
-            return el
-        elif el == ('before',):
-            return ('after',)
-        elif el == ('after',):
-            return ('before',)
-        else:
-            raise ValueError('This should not happen')
+def reverse(el):
+    if el == ('during',):
+        return el
+    elif el == ('before',):
+        return ('after',)
+    elif el == ('after',):
+        return ('before',)
+    else:
+        raise ValueError(f'el is {el}. This should not happen')
+
+def action_order_unique(classes, only_before=False):
     classes_uniq = []
     for el in classes:
         if el not in classes_uniq and ((el[0][1], el[0][0]), reverse(el[1])) not in classes_uniq:
-            classes_uniq.append(el)
+            if only_before and el[1] == ('after',):
+                classes_uniq.append(((el[0][1], el[0][0]), reverse(el[1])))
+            else:
+                classes_uniq.append(el)
     return classes_uniq
 
 
@@ -58,8 +62,18 @@ def class_keys_task1(string=True):
     else:
         return classes
 
+def class_to_string(class_, return_duplicate=False):
+    if return_duplicate:
+        return (class_[0][0][0] + class_[0][0][1] + ' ' + class_[1][0] + ' ' + class_[0][1][0] + class_[0][1][1],
+                class_[0][1][0] + class_[0][1][1] + ' ' + reverse(class_[1])[0] + ' ' + class_[0][0][0] + class_[0][0][1])
+    else:
+        return class_[0][0][0] + class_[0][0][1] + ' ' + class_[1][0] + ' ' + class_[0][1][0] + class_[0][1][1]
 
-def class_keys_task2(n=2, unique=True, string=True):
+def class_keys_task2(n=2, unique=True, string=True, only_before=False):
+    """
+    params:
+        only_before (bool): if True, convert all "after" to "before".
+    """
     action_sets = list(product(ACTION_CLASSES, repeat=n))
     # all orderings
     orderings = list(product(ORDERING, repeat=(n-1)))
@@ -67,14 +81,29 @@ def class_keys_task2(n=2, unique=True, string=True):
     classes = list(product(action_sets, orderings))
     if unique:
         # Remove classes such as "X before Y" when "Y after X" already exists in the data
-        classes = action_order_unique(classes)
+        classes = action_order_unique(classes, only_before=only_before)
 
     if string:
-        classes = [x[0][0][0] + x[0][0][1] + ' ' + x[1][0] + ' ' + x[0][1][0] + x[0][1][1] for x in classes]
+        classes = [class_to_string(x, False) for x in classes]
         return classes
 
     else:
         return classes
+
+
+def class_keys_to_labels(class_keys_tuple):
+    """Get a dictionary of which keys are class keys in string, and values are labels in integer.
+    This includes the reversed duplicates, so two of the keys indicate one label value.
+    exception: x during x == x during x so only one indicates one label.
+    """
+
+    class_keys_to_labels_dict = {}
+    for i, class_key in enumerate(class_keys_tuple):
+        class_key1, class_key2 = class_to_string(class_key, return_duplicate=True)
+        class_keys_to_labels_dict[class_key1] = i
+        class_keys_to_labels_dict[class_key2] = i
+
+    return class_keys_to_labels_dict
 
 
 def main():
---
+++ video_datasets_api/cater/generate_labels_from_scenes.py
@@ -0,0 +1,212 @@
+import json
+from .class_keys import ACTION_CLASSES, ORDERING, _BEFORE, _AFTER, _DURING, class_keys_task2, class_keys_to_labels
+from .definitions import NUM_CLASSES_TASK2
+
+import glob
+import os
+import numpy as np
+#from tqdm import tqdm
+import logging
+import subprocess
+from functools import partial
+import re
+import pickle
+from collections import defaultdict, OrderedDict
+from itertools import permutations, product, combinations
+import multiprocessing as mp
+import math
+import copy
+
+
+def get_ordering(act1_time, act2_time):
+    if act1_time[1] <= act2_time[0]:
+        # act1 should finish before act2 starts
+        return _BEFORE
+    elif act2_time[1] <= act1_time[0]:
+        # act1 should start after act2 ends
+        return _AFTER
+    else:
+        # We define everything else as "during", though it might be partly
+        # overlapped etc
+        return _DURING
+
+
+def satisfy_action_class(action_class, actions_set):
+    action_class_ents, action_class_ord = action_class
+    assert len(action_class_ents) == len(actions_set), \
+        'Must be same number of actions'
+    # The action set must contain the exact objects in exact motion
+    for action_class_ent, action in zip(action_class_ents, actions_set):
+        if (action_class_ent[0] != action[0] or
+                action_class_ent[1] != action[1][0]):
+            return False
+    # Now need to make sure the intervals make sense w.r.t the relation
+    for i in range(len(action_class_ord)):
+        if not get_ordering(
+                actions_set[i][1][2:],
+                actions_set[i + 1][1][2:]) == action_class_ord[i]:
+            return False
+    return True
+
+
+# slow version of `generate_task2_labels_from_scenes()`
+def compute_active_labels(movements, objects, classes, n=2):
+    name_to_type = {el['instance']: el['shape'] for el in objects}
+    all_actions = []
+    for name, motions in movements.items():
+        for motion in motions:
+            all_actions.append((
+                name_to_type[name],
+                motion))
+    # Consider all n-length permutations of all_actions, and check if it
+    # fits any of the classes
+    this_lbl = set()
+    for (cls_id, action_class), actions_set in product(
+            enumerate(classes), permutations(all_actions, n)):
+        if satisfy_action_class(action_class, actions_set):
+            this_lbl.add(cls_id)
+    return this_lbl
+
+# faster version of `compute_active_labels()` but only works with task2 (len(classes) == 301 and n==2)
+def generate_task2_labels_from_scenes(movements, objects, class_keys_to_labels_dict):
+    n = 2
+    name_to_type = {el['instance']: el['shape'] for el in objects}
+    all_actions = []        # _no_op filtered out
+    for name, motions in movements.items():
+        for motion in motions:
+            if motion[0] != '_no_op':
+                all_actions.append((
+                    name,
+                    name_to_type[name],
+                    motion))
+
+    def actions_pair_to_class_keys_only_before(actions_pair):
+        order = get_ordering(actions_pair[0][2][2:], actions_pair[1][2][2:])
+        if order == _AFTER:
+            class_key = f"{actions_pair[1][1]}{actions_pair[1][2][0]} before {actions_pair[0][1]}{actions_pair[0][2][0]}"
+            return (actions_pair[1], actions_pair[0]), class_key
+        elif order == _BEFORE:
+            class_key = f"{actions_pair[0][1]}{actions_pair[0][2][0]} before {actions_pair[1][1]}{actions_pair[1][2][0]}"
+            return actions_pair, class_key
+        elif order == _DURING:
+            class_key = f"{actions_pair[0][1]}{actions_pair[0][2][0]} during {actions_pair[1][1]}{actions_pair[1][2][0]}"
+            return actions_pair, class_key
+        else:
+            raise NotImplementedError()
+
+    labels = set()
+    actions_in_charge = {}
+    for actions_pair in combinations(all_actions, n):
+        this_actions_pair, class_key = actions_pair_to_class_keys_only_before(actions_pair)
+        label = class_keys_to_labels_dict[class_key]
+        labels.add(label)
+        if label not in actions_in_charge.keys():
+            actions_in_charge[label] = []
+        actions_in_charge[label].append(actions_pair)
+
+    return labels, actions_in_charge
+
+def action_order_unique(classes):
+    def reverse(el):
+        if el == ('during',):
+            return el
+        elif el == ('before',):
+            return ('after',)
+        elif el == ('after',):
+            return ('before',)
+        else:
+            raise ValueError('This should not happen')
+    classes_uniq = []
+    for el in classes:
+        if el not in classes_uniq and ((el[0][1], el[0][0]), reverse(el[1])) not in classes_uniq:
+            classes_uniq.append(el)
+    return classes_uniq
+
+def main_single_scene(CATER_dir):
+    import time
+
+    scenes_dir = os.path.join(CATER_dir, 'max2action/scenes')
+    scene_path = os.path.join(scenes_dir, 'CATER_new_000012.json')
+    with open(scene_path, 'r', encoding='utf8') as scene_file:
+        scene = json.load(scene_file, object_hook=OrderedDict)
+
+    classes = class_keys_task2(string=False)
+
+    # slow way
+    start_time = time.time()
+    labels = compute_active_labels(scene['movements'], scene['objects'], classes)
+    elapsed_time = time.time() - start_time
+    print(sorted(list(labels)))
+    print(f'Elapsed time of the slow function from CATER: {elapsed_time}')
+
+    # fast way
+    class_keys_to_labels_dict = class_keys_to_labels(classes)
+    start_time = time.time()
+    labels, actions_in_charge = generate_task2_labels_from_scenes(scene['movements'], scene['objects'], class_keys_to_labels_dict)
+    elapsed_time = time.time() - start_time
+    labels = sorted(list(labels))
+    print(labels)
+    print(actions_in_charge[labels[0]])
+    print(f'Elapsed time of the fast function from video_datasets_api: {elapsed_time}')
+
+
+def main_all_scenes(CATER_dir):
+    import time
+    import tqdm
+
+    scenes_dir = os.path.join(CATER_dir, 'max2action/scenes')
+    lists_dir = os.path.join(CATER_dir, 'max2action/lists/actions_order_uniq')
+
+    # read lists
+    print("Reading lists..")
+    labels_from_lists = {}
+    with open(os.path.join(lists_dir, 'train.txt'), 'r') as split_file:
+        for line in split_file.readlines():
+            filename, labels = line.split(' ')
+            basename, _ = os.path.splitext(filename)
+            labels_list = list(map(int, labels.split(',')))
+            labels_from_lists[basename] = labels_list
+    with open(os.path.join(lists_dir, 'val.txt'), 'r') as split_file:
+        for line in split_file.readlines():
+            filename, labels = line.split(' ')
+            basename, _ = os.path.splitext(filename)
+            labels_list = list(map(int, labels.split(',')))
+            labels_from_lists[basename] = labels_list
+
+    classes = class_keys_task2(string=False)
+
+    # read scenes
+    print("Reading scenes..")
+    scenes = {}
+    for basename in labels_from_lists.keys():
+        scene_path = os.path.join(scenes_dir, basename + '.json')
+        with open(scene_path, 'r', encoding='utf8') as scene_file:
+            scenes[basename] = json.load(scene_file, object_hook=OrderedDict)
+
+    # slow way
+    print("Evaluating the slow function..")
+    start_time = time.time()
+    for basename, labels_lists in tqdm.tqdm(labels_from_lists.items()):
+        scene = scenes[basename]
+        labels_scenes = compute_active_labels(scene['movements'], scene['objects'], classes)
+        assert set(labels_lists) == labels_scenes, f"Labels generated does not match with labels provided in `lists`. {labels_lists} and {sorted(list(labels_scenes))}"
+
+    elapsed_time = time.time() - start_time
+    print(f'Elapsed time of the slow function from CATER: {elapsed_time}')
+
+    # fast way
+    print("Evaluating the fast function..")
+    class_keys_to_labels_dict = class_keys_to_labels(classes)
+    start_time = time.time()
+    for basename, labels_lists in tqdm.tqdm(labels_from_lists.items()):
+        scene = scenes[basename]
+        labels_scenes, actions_in_charge = generate_task2_labels_from_scenes(scene['movements'], scene['objects'], class_keys_to_labels_dict)
+        assert set(labels_lists) == labels_scenes, f"Labels generated does not match with labels provided in `lists`. {labels_lists} and {sorted(list(labels_scenes))}"
+
+    elapsed_time = time.time() - start_time
+    print(f'Elapsed time of the fast function from video_datasets_api: {elapsed_time}')
+
+if __name__ == '__main__':
+    CATER_dir = '/home/kiyoon/datasets/CATER'
+    #main_single_scene(CATER_dir)
+    main_all_scenes(CATER_dir)
