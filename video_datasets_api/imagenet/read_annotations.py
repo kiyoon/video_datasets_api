8aeaa9b45dd285e923626a730a29ffe53460b134
Kiyoon Kim <yoonkr33@gmail.com>
2021-06-22 03:49:03 -0400

    classid to labels removed newline (stripped)

--- video_datasets_api/imagenet/read_annotations.py
+++ video_datasets_api/imagenet/read_annotations.py
@@ -4,7 +4,7 @@ _SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 def class_id_to_label():
     classid_to_label = {}
     with open(os.path.join(_SCRIPT_DIR, 'class_id.txt'), 'r') as f:
-        for i, line in enumerate(f.readlines()):
+        for i, line in enumerate(f.read().splitlines()):
             classid_to_label[line] = i
     return classid_to_label
 
