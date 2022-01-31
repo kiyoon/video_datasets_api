7a50ae1a8eff24e0b10221f5c8e655f4f0f06b74
Kiyoon Kim <kiyoon.kim@ed.ac.uk>
2022-01-31 19:18:19 +0000

    fix pandas parsing error

--- video_datasets_api/epic_kitchens_100/read_annotations.py
+++ video_datasets_api/epic_kitchens_100/read_annotations.py
@@ -13,19 +13,19 @@ def epic_narration_id_to_unique_id(train_pkl, val_pkl, test_pkl = None):
     with open(train_pkl, 'rb') as f:
         train_labels = pickle.load(f)
     
-    narration_ids_sorted = sorted(train_labels.narration_id, key = sort_narration_id)
+    narration_ids_sorted = sorted(train_labels.index, key = sort_narration_id)
 
     with open(val_pkl, 'rb') as f:
         val_labels = pickle.load(f)
 
-    val_narration_ids = sorted(val_labels.narration_id, key = sort_narration_id)
+    val_narration_ids = sorted(val_labels.index, key = sort_narration_id)
     narration_ids_sorted.extend(val_narration_ids)
 
     if test_pkl is not None:
         with open(test_pkl, 'rb') as f:
             test_labels = pickle.load(f)
 
-        test_narration_ids = sorted(test_labels.narration_id, key = sort_narration_id)
+        test_narration_ids = sorted(test_labels.index, key = sort_narration_id)
         narration_ids_sorted.extend(test_narration_ids)
 
     narration_id_to_video_id = {narration_id: uid for uid, narration_id in enumerate(narration_ids_sorted)}
@@ -42,7 +42,7 @@ def get_verb_uid2label_dict(train_pkl, val_pkl, narration_id_to_video_id):
     
     num_videos = len(train_labels.index)
     for index in range(num_videos):
-        narration_id = train_labels.narration_id.iloc[index]
+        narration_id = train_labels.index.iloc[index]
         uid = narration_id_to_video_id[narration_id]
         verb_label = train_labels.verb_class.iloc[index]
 
@@ -53,7 +53,7 @@ def get_verb_uid2label_dict(train_pkl, val_pkl, narration_id_to_video_id):
 
     num_videos = len(val_labels.index)
     for index in range(num_videos):
-        narration_id = val_labels.narration_id.iloc[index]
+        narration_id = val_labels.index.iloc[index]
         uid = narration_id_to_video_id[narration_id]
         verb_label = val_labels.verb_class.iloc[index]
 
