8540d76b15e472f1bd8635194375a93d9d4faa3f
Kiyoon Kim <yoonkr33@gmail.com>
2022-02-02 13:40:06 +0000

    errata checksum check

--- epic_100_tools/check_md5.sh
+++ epic_100_tools/check_md5.sh
@@ -12,6 +12,10 @@ epic100_dir="$1"
 md5csv="$2"
 
 md5file=$(cat "$md5csv" | grep ",100$" | awk -F, '{print $1" "$2}')
+md5file_errata=$(cat "$md5csv" | grep ",errata$" | awk -F, '{print $1" "$2}')
 
 cd "$epic100_dir"
 md5sum -c <(echo "$md5file")
+
+echo "Checking errata"
+md5sum -c <(echo "$md5file_errata")
