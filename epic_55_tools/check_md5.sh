0cff7421ceb58b12d79af98f004bc58561dc8333
Kiyoon Kim <yoonkr33@gmail.com>
2022-02-02 13:33:50 +0000

    epic check md5

---
+++ epic_55_tools/check_md5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]
+then
+	echo "Usage: $0 [EPIC_KITCHENS_55_dir] [md5.csv]"
+	echo "Download md5.csv from https://raw.githubusercontent.com/epic-kitchens/epic-kitchens-download-scripts/master/data/md5.csv"
+	exit 1
+fi
+
+
+epic100_dir="$1"
+md5csv="$2"
+
+md5file=$(cat "$md5csv" | grep ",55$" | awk -F, '{print $1" "$2}')
+
+cd "$epic100_dir"
+md5sum -c <(echo "$md5file")
