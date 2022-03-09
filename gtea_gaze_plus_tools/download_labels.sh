e3d7434234645846934080e376c93e3d8ed9d1db
Kiyoon Kim <yoonkr33@gmail.com>
2022-03-09 17:25:29 +0000

    gtea downloader

---
+++ gtea_gaze_plus_tools/download_labels.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]
+then
+	echo "Usage: $0 [output_dir]"
+	exit 1
+fi
+
+output_dir="$1"
+wget --content-disposition https://www.dropbox.com/s/ms1z1z8iqi997yp/GTEA_Gaze_Plus_labels_cleaned.zip?dl=1 -P "$output_dir"
+unzip "$output_dir/GTEA_Gaze_Plus_labels_cleaned.zip" -d "$output_dir"
+rm "$output_dir/GTEA_Gaze_Plus_labels_cleaned.zip"
