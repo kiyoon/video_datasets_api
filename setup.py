37b2630f9c127647dc984861de2c8ea6c14f1426
Kiyoon Kim <yoonkr33@gmail.com>
2022-04-05 18:51:53 +0100

    packages fix, now possible to install directly from github

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """setup script for video_datasets_api package."""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "video_datasets_api",
@@ -9,7 +9,8 @@ setup(
     author_email='kiyoon.kim@ed.ac.uk',
     description = "Video datasets' annotation parser and etc.",
     url = "https://github.com/kiyoon/video_datasets_api",
-    packages=['video_datasets_api'],
+    packages=find_packages(),
+    #packages=['video_datasets_api'],
     #package_dir={'video_datasets_api': 'src'},
     python_requires='>=3.5',
     install_requires=['numpy>=1.16.0',
