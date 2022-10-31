3c235459a3102df2980271aefd1038a5e04f9f60
Kiyoon Kim <yoonkr33@gmail.com>
2022-10-31 20:45:48 +0000

    category for epic100

--- video_datasets_api/epic_kitchens_100/definitions.py
+++ video_datasets_api/epic_kitchens_100/definitions.py
@@ -1,2 +1,20 @@
 NUM_VERB_CLASSES = 97
 NUM_NOUN_CLASSES = 300
+
+
+# The ordering follows the official website's (https://epic-kitchens.github.io/2022) stats graph.
+verb_categories = ['retrieve', 'leave', 'clean', 'access', 'block', 'merge', 'split', 'manipulate', 'transition', 'monitor', 'sense', 'distribute', 'other']
+noun_categories = ['appliances', 'crockery', 'containers', 'cutlery', 'vegetables', 'cookware', 'utensils', 'furniture', 'cleaning', 'storage', 'baked goods and grains', 'spices and herbs and sauces', 'materials', 'meat and substitutes', 'rubbish', 'dairy and eggs', 'prepared food', 'hand', 'drinks', 'fruits and nuts', 'other']
+
+
+NUM_VERB_CATEGORIES = len(verb_categories)
+NUM_NOUN_CATEGORIES = len(noun_categories)
+
+verb_category_str_to_index = {}
+for idx, category in enumerate(verb_categories):
+    verb_category_str_to_index[category] = idx
+
+noun_category_str_to_index = {}
+for idx, category in enumerate(noun_categories):
+    noun_category_str_to_index[category] = idx
+
--- video_datasets_api/epic_kitchens_100/get_class_keys.py
+++ video_datasets_api/epic_kitchens_100/get_class_keys.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 
+from .definitions import verb_category_str_to_index, noun_category_str_to_index
+
 def EPIC100_get_class_keys(annotations_root_dir: str, verbnoun = 'verb'):
     """Get class keys (verb or noun) 
 
@@ -11,3 +13,25 @@ def EPIC100_get_class_keys(annotations_root_dir: str, verbnoun = 'verb'):
     
     class_keys = pd.read_csv(os.path.join(annotations_root_dir, f'EPIC_100_{verbnoun}_classes.csv'), quotechar='"', skipinitialspace=True)['key']
     return class_keys
+
+
+def EPIC100_get_class_label_to_category_index(annotations_root_dir: str, verbnoun = 'verb'):
+    """Get class groups (verb or noun) 
+
+    Returns:
+        pandas dataframe (list of strings)
+    """
+    assert verbnoun in ['verb', 'noun']
+    if verbnoun == 'verb':
+        category_str_to_index = verb_category_str_to_index
+    else:
+        category_str_to_index = noun_category_str_to_index
+    
+    classes = pd.read_csv(os.path.join(annotations_root_dir, f'EPIC_100_{verbnoun}_classes.csv'), quotechar='"', skipinitialspace=True)
+
+    class_to_category = []
+    for index, row in classes.iterrows():
+        assert index == int(row['id'])
+        class_to_category.append(category_str_to_index(row['category']))
+
+    return class_to_category
